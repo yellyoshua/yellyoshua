cd576fe38bff47f5ce1ef6197b52370731e722b1
yellyoshua <usuario1529yoshua@hotmail.com>
2021-11-07 17:41:44 -0500

    fixed problem with head & font optimization

--- app/next.config.js
+++ app/next.config.js
@@ -3,4 +3,5 @@ module.exports = {
     NEXT_PUBLIC_API_URI: process.env.NEXT_PUBLIC_API_URI,
     NEXT_PUBLIC_API_TOKEN: process.env.NEXT_PUBLIC_API_TOKEN,
   },
+  optimizeFonts: true,
 };
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,21 +1,8 @@
 import "tailwindcss/tailwind.css";
-import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <meta charSet="utf-8"/>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@3.0.6/css/line.css"></link>
-      <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet"></link>
-      <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&display=swap" rel="stylesheet"></link>
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
