17d2ca591cdd9d24ab180f3e8339a601e578dfa4
yellyoshua <usuario1529yoshua@hotmail.com>
2022-08-14 23:58:39 -0500

    add microsoft clarity as analytics

--- app/src/ui/Layout.tsx
+++ app/src/ui/Layout.tsx
@@ -36,6 +36,16 @@ export function Layout({ children, title = APP_NAME }: LayoutProps) {
 				src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7465243281843526'
 				crossOrigin='anonymous'
 			></script>
+			<script
+				type="text/javascript"
+				dangerouslySetInnerHTML={{__html: `
+					(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "d7wkdxyom1");
+				`}}
+			></script>
 			<style global jsx>{`
 				img {
 					pointer-events: none;
