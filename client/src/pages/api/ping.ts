9eb67f2e831a5754da3e7462ce65ee6407bbfcef
yellyoshua <usuario1529yoshua@hotmail.com>
2024-04-13 19:49:53 -0500

    Add ping function api

--- client/src/pages/api/ping.ts
+++ client/src/pages/api/ping.ts
@@ -2,4 +2,4 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(_: VercelRequest, response: VercelResponse) {
 	response.status(200).send('ok');
-};
+}
