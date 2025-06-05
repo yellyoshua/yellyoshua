642b7c154d4b0ec07b865c381b88add53d686423
yellyoshua <usuario1529yoshua@hotmail.com>
2025-06-05 01:42:37 -0500

    Improve typescript errors

--- v2/tailwind.config.mjs
+++ v2/tailwind.config.mjs
@@ -1,7 +1,4 @@
 
-// require('@tailwindcss/typography')
-import typography from '@tailwindcss/typography';
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
